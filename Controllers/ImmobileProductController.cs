341f11f29d012d388b2d66885eb1fa7a5018fe2c
Sedat <sedattungac12@gmail.com>
2022-03-03 18:00:50 +0300

    03.03.2022 Perşembe Gerekli Controller eklendi.

---
+++ Controllers/ImmobileProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SSY_Project.Models.Entity;
+
+namespace SSY_Project.Controllers
+{
+    public class ImmobileProductController : Controller
+    {
+        DbSahaSatisTakipEntities db = new DbSahaSatisTakipEntities();
+        // GET: ImmobileProduct
+        public ActionResult Index()
+        {
+            var value = db.Data_HareketsizUrun.ToList();
+            return View(value);
+        }
+    }
+}
---
+++ Controllers/RecentArrivalsController.cs
@@ -0,0 +1,20 @@
+﻿using SSY_Project.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSY_Project.Controllers
+{
+    public class RecentArrivalsController : Controller
+    {
+        DbSahaSatisTakipEntities db = new DbSahaSatisTakipEntities();
+        // GET: RecentArrivals
+        public ActionResult Index()
+        {
+            var value = db.Data_SonGelenler.OrderByDescending(x => x.IND).ToList();
+            return View(value);
+        }
+    }
+}
