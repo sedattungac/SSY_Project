45b4cc7b03ff67408e4248b73653b3efc62edbfd
Sedat <sedattungac12@gmail.com>
2022-03-09 16:01:10 +0300

    analiz düzenlemeler yapıldı.

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -19,19 +19,27 @@ namespace SSY_Project.Controllers
         }
         public ActionResult CustomerInfo(string id)
         {
-            var customerList = db.data_caribilgi.Where(x=>x.FIRMAKODU==id).ToList();
+            var customerList = db.data_caribilgi.Where(x => x.FIRMAKODU == id).ToList();
+            ViewBag.id = id;
             return View(customerList);
         }
         public ActionResult CustomerDetail(string id)
-        { 
+        {
             var value = db.data_caridetay.Where(x => x.FIRMAKODU == id).ToList();
+            ViewBag.id = id;
             return View(value);
         }
         public ActionResult OldBillofSale(string id)
         {
             var value = db.data_satfat.Where(x => x.FIRMAKODU == id).ToList();
+            ViewBag.id = id;
             return View(value);
         }
-
+        public ActionResult LastCollection(string id)
+        {
+            var value = db.data_gectahsilat.Where(x => x.IND == id).ToList();
+            ViewBag.id = id;
+            return View(value);
+        }
     }
 }
